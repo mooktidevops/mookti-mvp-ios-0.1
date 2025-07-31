01b96cce8e86de71f5bcb231de192f88f7023fd6
Matthew Kumar <mm_kumar@icloud.com>
2025-07-31 04:07:56 -0700

    Initial Commit

---
+++ Mookti MVP/Mookti_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mookti_MVPApp.swift
+//  Mookti MVP
+//
+//  Created by Emmy Kumar on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Mookti_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mookti MVPTests/Mookti_MVPTests.swift
@@ -0,0 +1,16 @@
+//
+//  Mookti_MVPTests.swift
+//  Mookti MVPTests
+//
+//  Created by Emmy Kumar on 7/31/25.
+//
+
+import Testing
+
+struct Mookti_MVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
